21b618aca2bf55f27501fd67087c02fb7b746e4b
nabireum <anaestabanez.uni@gmail.com>
2026-02-23 11:53:20 -0300

    Update titles and hints for enigmas

--- client/src/data/enigmas.ts
+++ client/src/data/enigmas.ts
@@ -10,15 +10,15 @@ export interface Enigma {
 export const ENIGMAS_LISTA: Enigma[] = [
   {
     slug: 'cofre',
-    title: '',
+    title: 'O COMEÇO DO FIM',
     question: `Se está lendo isso, então provavelmente gostaria de saber a verdade. Mas preciso ter certeza de que a pessoa errada não chegou até aqui por engano. Se teve acesso àqueles documentos, então já sabe o que fazer. Prove que meu segredo está seguro contigo.`,
-    hint: 'A resposta está nos documentos que você encontrou anteriormente',
+    hint: 'A resposta ainda está com você. O fim de algo pode significar um novo começo também, basta entender que há segredos muito mais bem guardados.',
     answer: '7Q!mZ9@F#2KxR$A8',
     nextSlug: 'festa',
   },
   {
     slug: 'festa',
-    title: '',
+    title: 'PASSARINHO AZUL',
     question: `A Primeira vez. Você se lembra?`,
     hint: 'Se você não estava presente, pode vasculhar as memórias digitando uma hashtag especifica.',
     answer: 'FarofaDaBlythe',
